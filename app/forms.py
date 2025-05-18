db72b1e0471cb54f1d9aa7b1516f3957176fdfb5
Asilbek <asilbek.sadullayev000@example.com>
2025-05-18 13:38:40 +0500

    requiremnt

--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import EmployerAd, JobAd
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
 
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Foydalanuvchi nomi'}),
+            'email': forms.EmailInput(attrs={'class': 'input', 'placeholder': 'Email'}),
+            'password1': forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Parol'}),
+            'password2': forms.PasswordInput(attrs={'class': 'input', 'placeholder': 'Parolni tasdiqlang'}),
+        }
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None,
+        }
 class EmployerAdForm(forms.ModelForm):
     class Meta:
         model = EmployerAd
